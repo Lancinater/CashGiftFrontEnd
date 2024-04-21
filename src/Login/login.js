1539e97da9a8ca70cbe3409d6682c3c7c2ba346e
xuanjian zhang <zwordgize@gmail.com>
2024-04-21 17:22:28 +1000

    login logic implemented

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,16 +1,49 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleChange(event){
+        const {id, value} = event.target;
+        if (id === 'username'){
+            setUsername(value);
+        } else if (id === 'password'){
+            setPassword(value);
+        }
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+
+        fetch('http://localhost:8080/api/v1/cashGifts/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify({username, password})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Login successful:', data);
+            // add page redirecting here
+        })
+        .catch(error => {
+            console.error('Error during login:', error);
+        })
+    }
+
   return (
     <div className="vh-100 d-flex justify-content-center align-items-center">
-      <form className="w-25">
+      <form className="w-25" onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">Username</label>
-          <input type="email" className="form-control" id="email" />
+          <label htmlFor="username" className="form-label">Username</label>
+          <input type="username" className="form-control" id="username" value={username} onChange={handleChange}/>
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" id="password" />
+          <input type="password" className="form-control" id="password" value={password} onChange={handleChange}/>
         </div>
         <button type="submit" className="btn btn-primary">Log In</button>
       </form>
