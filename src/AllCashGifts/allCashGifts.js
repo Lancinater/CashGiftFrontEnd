f3e474fef5480a898919dc6e5f1a0a99d83d536a
Lancinater <zwordgize@gmail.com>
2024-08-27 20:50:06 +1000

    allcashgifts can be accessed and will show up all the cashgifts

--- src/AllCashGifts/allCashGifts.js
+++ src/AllCashGifts/allCashGifts.js
@@ -5,19 +5,26 @@ import { useNavigate } from 'react-router-dom';
 
 export default function AllCashGifts(){
     let navigate = useNavigate();
-    const [cashGifts, setCashGifts] = useState({});
+    const [cashGifts, setCashGifts] = useState([]);
 
     function handleGoback(){
         navigate('/menu')
     }
 
     useEffect (()=>{
-      fetch('http://localhost:8080/api/v1/cashGifts')
-        .then(Response => {
-          if(!Response.ok){
+      let token = localStorage.getItem("token");
+      let headerString = 'Bearer ' + token;
+      fetch('http://localhost:8080/api/v1/cashGifts', {
+        method: 'GET',
+        headers: {
+            'Authorisation': headerString
+        },
+      })
+        .then(response => {
+          if(!response.ok){
             throw new Error('Network response was not ok');
           }
-          return Response.json();
+          return response.json();
         })
         .then(data=>{
           setCashGifts(data)
@@ -35,7 +42,16 @@ export default function AllCashGifts(){
         <div className="display-4 text-primary">
           All Cash Gifts
         </div>
-        
+        <div>
+          CashGifts:
+          {cashGifts.length > 0?(
+            <ul>
+              {cashGifts.map((gift,index)=>(
+                <li key={index}>Name: {gift.name} Amount: {gift.amount}</li>
+              ))}
+            </ul>
+          ):<p>Loading cashgifts.......</p>}
+        </div>
         <div id="button">
           <button onClick={handleGoback} className="btn btn-primary">Go back</button>
         </div>
