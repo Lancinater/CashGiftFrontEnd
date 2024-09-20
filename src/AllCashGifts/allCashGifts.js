6177bb194485d772c624977ef2f97e759c984582
Lancinater <zwordgize@gmail.com>
2024-09-20 23:49:23 +1000

    pagination and styles of table added

--- src/AllCashGifts/allCashGifts.js
+++ src/AllCashGifts/allCashGifts.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import './allCashGifts.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { json, useNavigate } from 'react-router-dom';
-import { flexRender, getCoreRowModel, getFilteredRowModel, useReactTable } from "@tanstack/react-table";
+import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
 import { Box } from "@chakra-ui/react";
 import { header } from "framer-motion/client";
 
@@ -42,10 +42,12 @@ export default function AllCashGifts(){
       {
         header: "Name",
         accessorKey: "name",
+        size: 200,
       },
       {
         header: "Amount",
         accessorKey: "amount",
+        size: 150,
       }
     ];
 
@@ -55,6 +57,7 @@ export default function AllCashGifts(){
       data: cashGifts.length > 0?cashGifts:[],
       getCoreRowModel: getCoreRowModel(),
       getFilteredRowModel: getFilteredRowModel(),
+      getPaginationRowModel: getPaginationRowModel(),
     });
     
     console.log(table.getHeaderGroups());
@@ -86,29 +89,37 @@ export default function AllCashGifts(){
 
     <div className="main-page d-flex container-fluid justify-content-center align-items-center" style={{ height: "100vh" }}>
       <div className="text-center">
-        <div className="display-4 text-primary">
+        <div className="display-4 text-primary" id="title">
           All Cash Gifts
         </div>
         
-        <table className="table" width={table.getTotalSize()}>
+        <table className="table table-hover table-primary">
           {table.getHeaderGroups().map(headerGroup => (
             <thead>
               <tr>
                 {headerGroup.headers.map(column => (
-                  <th>{column.column.columnDef.header}</th>
+                  <th key={column.id} style={{width: `${column.column.columnDef.size}px`}}>{column.column.columnDef.header}</th>
                 ))}
               </tr>
             </thead>
           ))}
-          {table.getRowModel().rows.map((row) => (
-            <tr>
-              {row.getVisibleCells().map(cell => (
-                <td>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
-              ))}
-            </tr>
-          ))}
+
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr>
+                {row.getVisibleCells().map(cell => (
+                  <td key={cell.id} style={{ width: `${cell.column.columnDef.size}px` }}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
         </table>
 
+        <div className="pageButtons">
+          <button onClick={() => table.setPageIndex(0)} className="btn btn-primary page-btn">First Page</button>
+          <button onClick={() => table.previousPage()} className="btn btn-primary page-btn">Previous Page</button>
+          <button onClick={() => table.nextPage()} className="btn btn-primary page-btn">Next Page</button>
+        </div>
 
         <div id="button">
           <button onClick={handleGoback} className="btn btn-primary">Go back</button>
